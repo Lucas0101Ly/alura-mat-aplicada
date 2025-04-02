2d6dbeb6770ce0f1f8256759c5e889163bf4f07f
Lucas0101Ly <132003590+Lucas0101Ly@users.noreply.github.com>
2025-04-02 10:43:30 -0300

    .

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-25T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-25T00:00:00");
-const tempoObjetivo3 = new Date("2025-12-25T00:00:00");
-const tempoObjetivo4 = new Date("2025-12-25T00:00:00");
+const tempoObjetivo2 = new Date("2025-8-25T00:00:00");
+const tempoObjetivo3 = new Date("2025-9-25T00:00:00");
+const tempoObjetivo4 = new Date("2025-11-25T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
